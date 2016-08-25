302db3c7e3d7c382de4c53cf957af1c86cfd34c6
krzys-h <krzys_h@interia.pl>
2016-08-25 19:59:01 +0200

    Add different process sizes

--- Assets/ProcessObject.cs
+++ Assets/ProcessObject.cs
@@ -14,8 +14,18 @@ public class ProcessObject : MonoBehaviour {
 	}
 
 	void Start () {
+		InvokeRepeating ("UpdateSize", 0, 0.1f);
 	}
 
 	void Update () {
+	}
+
+	void UpdateSize() {
+		if (process == null)
+			return;
+		float x = ((float)process.VirtualMemorySize64) / 1024 / 1024 / 1024;
+		if (x > 15)
+			x = 15;
+		this.transform.localScale = new Vector3 (x, x, x);
 	}
 }
