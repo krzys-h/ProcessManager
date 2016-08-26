0cc21a51c003819e5d15f93c0fdf808c8ec927a4
krzys-h <krzys_h@interia.pl>
2016-08-26 15:54:59 +0200

    Rotate text towards player

---
+++ Assets/RotateTowardsPlayer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateTowardsPlayer : MonoBehaviour {
+
+	Transform player;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find ("FPSController").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt(2 * transform.position - player.position);
+	}
+}
